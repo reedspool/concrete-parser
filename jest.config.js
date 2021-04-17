0cc69cc6594cf0ffda2c9ceb656d5226c3afb41d
Reed Spool <reedwith2es@gmail.com>
2021-04-17 14:38:11 -0600

    Move header args into subheadings, and add comments

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
+// =jest.config.js=
+// :PROPERTIES:
+// :header-args: :comments both :tangle jest.config.js
+// :END:
+
+// Because my custom package =xstate-jest-tools= is not Babel-transformed yet, this project must specifically inform Jest's Babel transformer to *not* ignore it, and transform it as well:
+
+
+// [[file:README.org::*=jest.config.js=][=jest.config.js=:1]]
 module.exports = {
     transformIgnorePatterns: [
         "node_modules/(?!xstate-jest-tools)"
     ]
 }
+// =jest.config.js=:1 ends here
