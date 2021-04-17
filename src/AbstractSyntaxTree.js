ec35bdb38379acb124de1193f120f692bdb7b318
Reed Spool <reedwith2es@gmail.com>
2021-04-17 10:58:35 -0600

    Transpose AST to es6 class

--- src/AbstractSyntaxTree.js
+++ src/AbstractSyntaxTree.js
@@ -1,13 +1,17 @@
 // #+TITLE: Abstract Syntax Tree
 // #+PROPERTY: header-args    :comments both :tangle ../src/AbstractSyntaxTree.js
 
+// Factory wrapper because I don't like =new= keyword.
+
 
 // [[file:../literate/AbstractSyntaxTree.org::+begin_src js][No heading:1]]
-export const AbstractSyntaxTree = function () {
-    return new AST();
-}
-
-function AST() {
-    this.root = true;
-}
+export const AbstractSyntaxTree = (...args) => new _AbstractSyntaxTree(...args);
 // No heading:1 ends here
+
+// [[file:../literate/AbstractSyntaxTree.org::+begin_src js][No heading:2]]
+class _AbstractSyntaxTree {
+    constructor() {
+        this.root = true;
+    }
+}
+// No heading:2 ends here
