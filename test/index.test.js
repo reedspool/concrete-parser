c066691df6cbef48912b7c994952a90bc6364724
Reed Spool <reedwith2es@gmail.com>
2021-04-19 15:47:24 -0600

    Expose Block.Category so we can determine whether a block is a value or an op from the interpreter

--- test/index.test.js
+++ test/index.test.js
@@ -2,8 +2,9 @@
 
 
 // [[file:../literate/IndexTests.org::*Preamble][Preamble:1]]
-import { parseFile } from "../src/index";
+import { parseFile, Category } from "../src/index";
 import * as Parser from "../src/Parser";
+import * as Block from "../src/Block";
 // Preamble:1 ends here
 
 // Tests
@@ -13,5 +14,7 @@ import * as Parser from "../src/Parser";
 it("Exposes everything it needs to.", () => {
     expect(parseFile).toBeDefined();
     expect(parseFile).toBe(Parser.parseFile);
+    expect(Category).toBeDefined();
+    expect(Category).toBe(Block.Category);
 })
 // Tests:1 ends here
